1469471d92082ee5a3d35117cd971e65ee993063
syuzichachyan <kchachyan@gmail.com>
2018-08-11 21:08:49 +0400

    [add] recipes component

--- src/components/recipes/index.js
+++ src/components/recipes/index.js
@@ -4,3 +4,43 @@ import Recipe from '../recipe';
 import injectSheet from 'react-jss';
 import styles from './style';
 
+class Recipes extends Component {
+    componentDidMount() {
+        const { includes, excludes, getRecipes } = this.props;
+        getRecipes(excludes, includes);
+    }
+    static propTypes = {
+        includes: PropTypes.array,
+        excludes: PropTypes.array,
+        getRecipes: PropTypes.func,
+        isRecipesFetching: PropTypes.bool,
+        recipes: PropTypes.array,
+        classes: PropTypes.object
+    };
+    static defaultProps = {
+        isRecipesFetching: false,
+        recipes: []
+    };
+
+    render() {
+        const { isRecipesFetching, recipes, classes } = this.props;
+        if (isRecipesFetching === false) {
+            return (
+                <div className={classes.recipes}>
+                    {recipes.map(item =>
+                        item.hits.map(recipe => {
+                            return (
+                                <Recipe
+                                    recipe={recipe.recipe}
+                                    key={recipe.recipe.url}
+                                />
+                            );
+                        })
+                    )}
+                </div>
+            );
+        } else return <div>loading</div>;
+    }
+}
+
+export default injectSheet(styles)(Recipes);
