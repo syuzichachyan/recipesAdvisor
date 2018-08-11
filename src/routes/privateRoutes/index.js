1881851cf4ad8b3509ed4b4845a49b7dfb828894
syuzichachyan <kchachyan@gmail.com>
2018-08-11 21:03:45 +0400

    [add] privateRoute

--- src/routes/privateRoutes/index.js
+++ src/routes/privateRoutes/index.js
@@ -1,2 +1,27 @@
 import React, { Component } from 'react';
 import { Redirect, Route } from 'react-router-dom';
+
+class PrivateRoute extends Component {
+    render() {
+        const { component: Component, isAuth, ...rest } = this.props;
+        return (
+            <Route
+                {...rest}
+                render={props =>
+                    isAuth ? (
+                        <Component {...props} />
+                    ) : (
+                        <Redirect
+                            to={{
+                                pathname: '/',
+                                state: { from: props.location }
+                            }}
+                        />
+                    )
+                }
+            />
+        );
+    }
+}
+
+export default PrivateRoute;
