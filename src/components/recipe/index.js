8d3f05acab46e511d5246f0f19c6c61168ef793c
syuzichachyan <kchachyan@gmail.com>
2018-08-11 21:09:39 +0400

    [add] recipe component

--- src/components/recipe/index.js
+++ src/components/recipe/index.js
@@ -3,4 +3,20 @@ import PropTypes from 'prop-types';
 import injectSheet from 'react-jss';
 import styles from './style';
 
+class Recipe extends Component {
+    static propTypes = {
+        recipe: PropTypes.object.isRequired,
+        classes: PropTypes.object
+    };
+    render() {
+        const { recipe, classes } = this.props;
+        return (
+            <div className={classes.recipe}>
+                <img src={recipe.image} />
+                <h3>{recipe.label}</h3>
+            </div>
+        );
+    }
+}
 
+export default injectSheet(styles)(Recipe);
