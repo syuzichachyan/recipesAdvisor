f7a8426db4ac71c5124b9fc4c7b68a56b2b5cdca
syuzichachyan <kchachyan@gmail.com>
2018-08-11 21:06:19 +0400

    [add] recipes container

--- src/containers/recipes/index.js
+++ src/containers/recipes/index.js
@@ -1,3 +1,21 @@
 import { connect } from 'react-redux';
 import { getRecipes } from '../../actions';
 import Recipes from '../../components/recipes';
+
+const mapStateToProps = state => {
+    return {
+        isRecipesFetching: state.isRecipesFetching,
+        recipes: state.recipes
+    };
+};
+const mapDispatchToProps = dispatch => {
+    return {
+        getRecipes: (excludes, includes) =>
+            dispatch(getRecipes(excludes, includes))
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Recipes);
