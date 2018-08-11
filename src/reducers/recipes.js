c5dedcc8fc86714f27eff63bebf7d06330f153aa
syuzichachyan <kchachyan@gmail.com>
2018-08-11 21:05:25 +0400

    [add] recipes reducer

--- src/reducers/recipes.js
+++ src/reducers/recipes.js
@@ -4,3 +4,30 @@ import {
     RECIPES_FETCHING
 } from '../constants';
 
+const initialStateIsRecipesFetching = false;
+export const isRecipesFetching = (
+    state = initialStateIsRecipesFetching,
+    action
+) => {
+    switch (action.type) {
+        case RECIPES_FETCHING:
+            return true;
+        case RECIPES_FETCHING_SUCCESS:
+            return false;
+        case RECIPES_FETCHING_FAILURE:
+            return false;
+        default:
+            return state;
+    }
+};
+const initialStateForRecipes = [];
+export const recipes = (state = initialStateForRecipes, action) => {
+    switch (action.type) {
+        case RECIPES_FETCHING_SUCCESS:
+            return [...state, action.payload];
+        case RECIPES_FETCHING_FAILURE:
+            return initialStateForRecipes;
+        default:
+            return state;
+    }
+};
