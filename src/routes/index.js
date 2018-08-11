32caa86997c2e04e8c3fcb7eb93e6500b3027a06
syuzichachyan <kchachyan@gmail.com>
2018-08-11 21:04:27 +0400

    [add] Routers Component

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,3 +2,19 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Profile from '../components/profile';
 
+class Routers extends Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route
+                        path="/profile"
+                        render={props => <Profile {...props} />}
+                    />
+                </Switch>
+            </Router>
+        );
+    }
+}
+
+export default Routers;
