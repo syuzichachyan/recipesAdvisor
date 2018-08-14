c9fa0e3287cb76e9cbf85952c4609eee392d88cc
RobMin <robertminasyan01@gmail.com>
2018-08-14 20:19:41 +0400

    [fix] Router remove [add] jss

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,53 +1,44 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import Recipes from '../../containers/recipes';
 import Header from '../../containers/Header';
 import HealthyRecipes from '../../containers/HealthyRecipes';
 import SpecialDiets from '../../containers/SpetialDiets';
 import { urlToProperty } from 'query-string-params';
+import styles from './styles';
+import injectSheet from 'react-jss';
 
 class Profile extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <React.Fragment>
-        <Router>
-          <div>
-            <Header />
-            <Link to={`${this.props.match.url}/health?label=alcohol-free`}>
-              Alcohol-Free
-            </Link>
-            <Link to={`${this.props.match.url}/health?label=vegan`}>Vegan</Link>
-            <Route
-              exact
-              path="/profile"
-              render={props => <Recipes {...props} />}
-            />
-            <Route
-              path="/profile/health"
-              render={({ location }) => (
-                <HealthyRecipes
-                  label={urlToProperty(location.search).label[0]}
-                />
-              )}
-            />
-            <Route
-              path="/profile/diet"
-              render={({ location }) => (
-                <SpecialDiets label={urlToProperty(location.search).label[0]} />
-              )}
-            />
-            <Link to={`${this.props.match.url}/health?label=alcohol-free`}>
-              Alcohol-Free
-            </Link>
-            <Link to={`${this.props.match.url}/health?label=vegan`}>Vegan</Link>
-            <Link to={`${this.props.match.url}/diet?label=balanced`}>
-              Vegan
-            </Link>
-          </div>
-        </Router>
-      </React.Fragment>
+      <div className={classes.main}>
+        <Header />
+        <Link to={`${this.props.match.url}/health?label=alcohol-free`}>
+          Alcohol-Free
+        </Link>
+        <Link to={`${this.props.match.url}/health?label=vegan`}>Vegan</Link>
+        <Route exact path="/profile" render={props => <Recipes {...props} />} />
+        <Route
+          path="/profile/health"
+          render={({ location }) => (
+            <HealthyRecipes label={urlToProperty(location.search).label[0]} />
+          )}
+        />
+        <Route
+          path="/profile/diet"
+          render={({ location }) => (
+            <SpecialDiets label={urlToProperty(location.search).label[0]} />
+          )}
+        />
+        <Link to={`${this.props.match.url}/health?label=alcohol-free`}>
+          Alcohol-Free
+        </Link>
+        <Link to={`${this.props.match.url}/health?label=vegan`}>Vegan</Link>
+        <Link to={`${this.props.match.url}/diet?label=balanced`}>Vegan</Link>
+      </div>
     );
   }
 }
 
-export default Profile;
+export default injectSheet(styles)(Profile);
