40475931e9883644355905dbdc92911ea2f17fce
Garik Apreyan <garik.apreyan@mail.ru>
2018-08-21 21:30:15 +0400

    [add] error glyph to ChangePassword component

--- src/components/ChangePassword/index.js
+++ src/components/ChangePassword/index.js
@@ -1,38 +1,43 @@
 import React, { Component } from 'react';
 import { Form, Field, reduxForm } from 'redux-form';
+import { Glyphicon } from 'react-bootstrap';
 import injectSheet from 'react-jss';
 import styles from './styles';
 
 class ChangePassword extends Component {
   render() {
-    const { handleSubmit, classes } = this.props;
+    const { handleSubmit, classes, reset, hasError } = this.props;
     return (
       <Form onSubmit={handleSubmit} className={classes.form}>
-        <label className={classes.field}>Old Password</label>
+        <label htmlFor={'oldPassword'} className={classes.field}>Old Password</label>
         <Field
           className={classes.field}
           component={'input'}
-          type={'text'}
+          type={'password'}
           name={'oldPassword'}
           placeholder={'Old Password'}
+          autoComplete={'oldPassword'}
         />
         <label className={classes.field}>New Password</label>
         <Field
           className={classes.field}
           component={'input'}
-          type={'text'}
-          name={'password'}
+          type={'password'}
+          name={'newPassword'}
           placeholder={'New Password'}
+          autoComplete={'newPassword'}
         />
         <label className={classes.field}>Repeat Password</label>
         <Field
           className={classes.field}
           component={'input'}
-          type={'text'}
+          type={'password'}
           name={'coPassword'}
           placeholder={'Repeat Password'}
+          autoComplete={'coPassword'}
         />
-        <button className={classes.field} type={'submit'}>
+        {hasError ? <Glyphicon glyph={'remove-sign'}/> : ''}
+        <button className={classes.field} type={'submit'} onClick={() => setTimeout(reset)}>
           Save
         </button>
       </Form>
--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -10,18 +10,29 @@ import injectSheet from 'react-jss';
 class Settings extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      hasError: false
+    };
 
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
     this.handlePreferencesChange = this.handlePreferencesChange.bind(this);
   }
 
-  static handlePasswordChange(changes) {
+  handlePasswordChange(changes) {
     console.log(changes);
+    const { newPassword, coPassword } = changes;
+    if(newPassword !== coPassword) {
+      this.setState({
+        hasError: true
+      });
+    }
   }
 
   handlePreferencesChange() {}
 
   render() {
     const { classes } = this.props;
+    const { hasError } =this.state;
     return (
       <div>
         <Header />
@@ -33,7 +44,7 @@ class Settings extends Component {
             className={classes.tabs}
           >
             <Tab eventKey={1} title={'Change Password'}>
-              <ChangePassword onSubmit={Settings.handlePasswordChange} />
+              <ChangePassword onSubmit={this.handlePasswordChange} hasError={hasError}/>
             </Tab>
             <Tab eventKey={2} title={'Change Preferences'}>
               <FoodPreferences />
