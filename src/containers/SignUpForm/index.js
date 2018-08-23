0b9638d06adfd4d94fba6cc4e3385be46fc426d4
Ghevond97 <ghevond_mirzakhanyan@edu.aua.am>
2018-08-24 00:53:21 +0400

    Container for the SignUp form

---
+++ src/containers/SignUpForm/index.js
@@ -0,0 +1,10 @@
+import {reduxForm} from "redux-form";
+import SignUp from "../../components/SignUpForm/index";
+import validate from "../../config/SignUpValidation/validtion";
+
+let SignUpForm = reduxForm({
+  form: "SignUp",
+  validate
+})(SignUp);
+
+export default SignUpForm;
