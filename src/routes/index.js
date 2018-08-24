2af5eebc5dbd72579c779145928467d2b0a0582f
syuzichachyan <kchachyan@gmail.com>
2018-08-24 12:19:43 +0400

    [add] routes for login and signUp

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Preferences from '../components/Preferences';
+import LoginForm from '../containers/LoginForm';
+import SignUpForm from '../containers/SignUpForm';
 import RoutesWithHeaderAndFooter from './routesWithHeaderAndFooter';
+
 
 class Routers extends Component {
   render() {
@@ -9,6 +12,8 @@ class Routers extends Component {
       <Router>
         <Switch>
           <Route path="/preferences" component={Preferences} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/signUp" component={SignUpForm} />
           <Route component={RoutesWithHeaderAndFooter} />
         </Switch>
       </Router>
