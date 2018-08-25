2d47b1aff142c736e4ea243716e83ad596ea6b01
ZaruhiHakobyan <kchachyan@gmail.com>
2018-08-25 13:03:36 +0400

    [change] api app_id and app_key

--- src/actions/specialDiets.js
+++ src/actions/specialDiets.js
@@ -45,7 +45,7 @@ export const getSpecialDiets = (labels, page = 0, q) => dispatch => {
   dispatch(specialDietsFetching());
 
   fetch(
-    `https://api.edamam.com/search?q=${q}&app_id=28fb7256&app_key=b3bccf42eb282f3b21740bf3fa472af3&from=${page *
+    `https://api.edamam.com/search?q=${q}&app_id=8d30ad7e&app_key=2e15423acdc14ff0c010ea43cd8c94e8&from=${page *
       count}&to=${count * (page + 1)}${connectedLabels ? connectedLabels : ''}${
       excludesFoods ? excludesFoods : ''
     }`
