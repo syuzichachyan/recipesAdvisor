db74576f120637ca99bbcaaf907b7f2bbb13939d
Garik <garik.apreyan@mail.ru>
2018-08-30 15:59:13 +0400

    [change] Recipe component

--- src/components/Recipe/index.js
+++ src/components/Recipe/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import { Col, Glyphicon } from 'react-bootstrap';
+import SmallCard from './SmallCard';
+import Card from './Card';
+import AnimatedCard from './AnimatedCard';
 import injectSheet from 'react-jss';
 import styles from './styles';
 
@@ -8,12 +10,7 @@ class Recipe extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      isRotated: false
-    };
-
     this.handleFavouriteClick = this.handleFavouriteClick.bind(this);
-    this.handleRotateClick = this.handleRotateClick.bind(this);
   }
 
   static propTypes = {
@@ -40,10 +37,6 @@ class Recipe extends Component {
     if (!jwt) {
       history.push('/Login');
     }
-    console.log({
-      favoriteId: recipe.uri,
-      recepte: { ...recipe, isFavourite: true }
-    });
     isFavourite
       ? deleteFetchFavourites(recipe.uri.slice(45), jwt)
       : fetchFavourites(
@@ -55,144 +48,10 @@ class Recipe extends Component {
         );
   }
 
-  handleRotateClick() {
-    const { isRotated } = this.state;
-    this.setState({
-      isRotated: !isRotated
-    });
-  }
-
   render() {
     const { recipe, classes } = this.props;
     const { isFavourite } = recipe;
-    const { isRotated } = this.state;
-    return <div style={{ height: '500px', width: '400px' }} />;
-    //  return (
-    {
-      /*<Col xs={6} md={4} className={classes.col}>*/
-    }
-    {
-      /*<div className={classes.scene}>*/
-    }
-    {
-      /*<div*/
-    }
-    {
-      /*className={`${classes.card} ${isRotated ? classes.isFlipped : ''}`}*/
-    }
-    {
-      /*onClick={this.handleRotateClick}*/
-    }
-    {
-      /*>*/
-    }
-    {
-      /*<div className={`${classes.cardFace} ${classes.cardFaceFront}`}>*/
-    }
-    {
-      /*<div id={'imageWrapper'} className={classes.imageWrapper}>*/
-    }
-    {
-      /*<form method="post" onSubmit={this.handleFavouriteClick}>*/
-    }
-    {
-      /*<button className={classes.favBtn}>*/
-    }
-    {
-      /*{isFavourite ? (*/
-    }
-    {
-      /*<Glyphicon glyph={'heart'} className={classes.glyph} />*/
-    }
-    {
-      /*) : (*/
-    }
-    {
-      /*<Glyphicon*/
-    }
-    {
-      /*glyph={'heart-empty'}*/
-    }
-    {
-      /*className={classes.glyph}*/
-    }
-    {
-      /*/>*/
-    }
-    {
-      /*)}*/
-    }
-    {
-      /*</button>*/
-    }
-    {
-      /*</form>*/
-    }
-    {
-      /*/!*<img*!/*/
-    }
-    {
-      /*/!*id={'plus'}*!/*/
-    }
-    {
-      /*/!*className={classes.plus}*!/*/
-    }
-    {
-      /*/!*src={'../../images/plus.png'}*!/*/
-    }
-    // {/*/!*/>*/*/}
-    {
-      /*<img*/
-    }
-    {
-      /*id={'mainImage'}*/
-    }
-    {
-      /*className={classes.mainImage}*/
-    }
-    {
-      /*src={recipe.image}*/
-    }
-    {
-      /*/>*/
-    }
-    {
-      /*<div className={classes.cline} />*/
-    }
-    {
-      /*</div>*/
-    }
-    {
-      /*<div className={classes.content}>*/
-    }
-    {
-      /*<h3 className={classes.title}>{recipe.label}</h3>*/
-    }
-    {
-      /*</div>*/
-    }
-    {
-      /*</div>*/
-    }
-    {
-      /*<div className={`${classes.cardFace} ${classes.cardFaceBack}`}>*/
-    }
-    {
-      /*<h3 className={classes.title}>{recipe.label}</h3>*/
-    }
-    {
-      /*</div>*/
-    }
-    {
-      /*</div>*/
-    }
-    {
-      /*</div>*/
-    }
-    {
-      /*</Col>*/
-    }
-    //);
+    return <Card />;
   }
 }
 
